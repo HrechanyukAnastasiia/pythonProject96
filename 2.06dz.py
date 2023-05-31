817d4b5c70ac16d2e45a9d9eb7675634d4becb7c
Hrechanyuk Anastasiia <anastasiya.grk@gmail.com>
2023-05-31 18:22:58 +0300

    1

---
+++ 2.06dz.py
@@ -0,0 +1,29 @@
+import logging
+class StudentProcessor:
+    def __init__(self):
+        self.students = []
+        self.logger = logging.getLogger("log")
+        self.logger.setLevel(logging.INFO)
+        self.logger.addHandler(logging.StreamHandler())
+    def add_student(self, student):
+        self.students.append(student)
+        self.logger.info(f"{student} був доданий до списку студентів")
+    def remove_student(self, student):
+        try:
+            self.students.remove(student)
+            self.logger.info(f"{student} був видалений зі списку студентів")
+        except:
+            print(f"Студента {student} не було знайде в списку студентів")
+    def process_students(self):
+        for student in self.students:
+            self.logger.info(f"{student} є студентом школи")
+
+name = StudentProcessor()
+student_1 = 'Настя'
+student_2 = 'Ната'
+student_3 = 'Ростік'
+student_4 = 'Назар'
+name.students = [student_1, student_2, student_3]
+name.add_student(student_4)
+name.remove_student(student_3)
+name.process_students()
